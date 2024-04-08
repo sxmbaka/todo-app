bdf2efc9045bac18f2be49c3e5eb1c74772b6053
Yatharth Singh <yatharth.singhhh@gmail.com>
2024-04-08 18:56:31 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TodoApp());
+}
+
+class TodoApp extends StatelessWidget {
+  const TodoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo List',
+      home: Scaffold(),
+    );
+  }
+}
