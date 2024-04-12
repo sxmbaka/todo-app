ae67310c4008fe4a17f5158fb0860a8fc7461881
Yatharth Singh <yatharth.singhhh@gmail.com>
2024-04-12 15:56:57 +0530

    added vscode config

--- lib/widgets/todo_list_tile.dart
+++ lib/widgets/todo_list_tile.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
